fd89df67815d100653ca5f1f0b6c065eb3813d47
Caleo Silva <caleosilva75@gmail.com>
2023-04-10 14:14:00 -0300

    Removendo imports desnecessários

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import NavBarFU from './componentes/NavBarFu';
-import EstoqueEntrada from './componentes/EstoqueEntrada';
-import EstoqueSaida from './componentes/EstoqueSaida';
-import FormAtualizarMed from './componentes/FormAtualizarMed';
-import FormCadastrarMed from './componentes/FormCadastrarMed';
 import TabelaMedicamentos from './componentes/TabelaMedicamentos';
-import InputBuscar from './componentes/InputBuscar';
-import TabelaEstoque from './componentes/TabelaEstoque';
-import TabelaDoadores from './componentes/TabelaDoadores';
-import TabelaPacientes from './componentes/TabelaPacientes';
 
 
 function App() {
